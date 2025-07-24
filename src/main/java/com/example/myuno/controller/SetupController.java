c9cf2f80656040cbe149995417dbff25b9995680
4rias01 <santiago.arias.rojas@correounivalle.edu.co>
2025-07-24 16:29:41 -0500

    new back button. New images of cards

---
+++ src/main/java/com/example/myuno/controller/SetupController.java
@@ -0,0 +1,26 @@
+package com.example.myuno.controller;
+
+import com.example.myuno.view.managers.AnimationsManager;
+import com.example.myuno.view.managers.CursorManager;
+import com.example.myuno.view.managers.Manager;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class SetupController {
+
+    @FXML
+    HBox hBoxExit;
+    @FXML
+    ImageView backImage;
+
+    @FXML
+    private void initialize() {
+        applyCustomEffect(hBoxExit);
+    }
+
+    private void applyCustomEffect(HBox hBox) {
+        Manager.applyAllEvents(hBox);
+    }
+}
